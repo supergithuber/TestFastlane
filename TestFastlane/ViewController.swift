e5f4f6e9ea6babf21a16beea909fba9e5eecb4e6
sivanwu <sivanwu>
2019-11-18 17:00:39 +0800

    update mini_magick version

--- TestFastlane/ViewController.swift
+++ TestFastlane/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        testDateFormatter()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +23,16 @@ class ViewController: UIViewController {
     }
 
 
+    func testDateFormatter(){
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale.init(identifier: "en_US_POSIX")
+//        dateFormatter.locale = Locale.init(identifier: "zh_CN@calendar=chinese")
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        let date = dateFormatter.date(from: "1993-01-31 16:56:34")
+        print(date!)
+//        let dateString = dateFormatter.string(from: Date())
+//        print(dateString)
+    }
 }
 
